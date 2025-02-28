2d80788abf5efbede6ec5d6ee7c4f310bdd8858d
Saud Alsaloum <saudalsaloum@Sauds-MacBook-Pro.local>
2025-02-28 13:45:09 +0300

    fdkljs

--- src/pages/Outcome.jsx
+++ src/pages/Outcome.jsx
@@ -2,6 +2,9 @@ import React, { useEffect, useState } from "react";
 import { Checkout, Footer, NavTech } from "../components";
 
 const Outcome = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <>
       <NavTech />
